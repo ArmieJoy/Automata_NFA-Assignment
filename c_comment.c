f9c260e5c7566da93e99fc149f0db2e57cc1a31b
ARMIE JOY TANGALIN <s.ajutangalin@usm.edu.ph>
2026-09-01 21:26:25 +0800

    w represents a C-style comment

--- c_comment.c
+++ c_comment.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-// Function to check if a string is a valid C-style comment
 bool isCComment(const char *str) {
     int len = strlen(str);
 
@@ -13,14 +12,20 @@ bool isCComment(const char *str) {
     if (!(str[0] == '/' && str[1] == '*')) return false;
     if (!(str[len-2] == '*' && str[len-1] == '/')) return false;
 
-    // Inside body can be any combination of a, *, /
+    // Ensure there are no extra "*/" inside before the end
+    for (int i = 2; i < len-2; i++) {
+        if (str[i] == '/' && str[i-1] == '*') {
+            return false; // found premature closing
+        }
+    }
+
     return true;
 }
 
 int main() {
     const char *tests[] = {
         "/*a*/", "/**/", "/***/", "/*aaa*aaa*/", "/*a/a*/",   // Accepted
-        "/**", "/**/a/**a*/", "/aaa/**/a/", "/*/", "/**a/", "/aaaa" // Rejected
+        "/**", "/**/a/*aa*/", "aaa/**/aa", "/*/", "/**a/", "//aaaa" // Rejected
     };
 
     int n = sizeof(tests)/sizeof(tests[0]);
